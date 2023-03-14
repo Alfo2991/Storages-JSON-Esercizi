c54a43ef5a38a1b744ce6f7b00455c097a5b96b4
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-03-14 16:15:05 +0100

    es1

--- newuser.js
+++ newuser.js
@@ -3,33 +3,36 @@
 function saveName() {
     const name = document.getElementById("name").value;
     localStorage.setItem("name", name);
-    alert("Nome salvato!");
+    
 
-    const nameList = document.getElementById("nameList");
-    const newNamelist = document.createElement('li');
-    newNamelist.innerHTML = name;
-    nameList.appendChild(newNamelist)
+    addNameToList(name);
+
+
 
 }
 
 function removeName() {
     localStorage.removeItem("name");
-    alert("Nome rimosso!");
+    
 
     const nameList = document.getElementById('nameList');
-    nameList.innerHTML = ".nomiSalvati"
+    nameList.innerHTML = ""
 
 }
-window.addEventListener('load', function(){
 
-const savedName = localStorage.getItem("name");
-if (savedName !== null) {
-    document.getElementById("name").value = savedName;
-
+function addNameToList(name) {
     const nameList = document.getElementById("nameList");
-    const newNamelist = document.createElement('li');
-    newNamelist.innerHTML = name;
+    const newNameList = document.createElement('li');
+    newNameList.innerHTML = name;
     nameList.appendChild(newNamelist)
 }
+window.addEventListener('load', function () {
+
+    const savedName = localStorage.getItem("name");
+    if (savedName !== null) {
+        document.getElementById("name").value = savedName;
+
+        addNameToList(savedName);
+    }
 
 })
